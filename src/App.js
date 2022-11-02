9cd18cb4209c34eb2d75c3793b6ceb35dde480a2
Pabasaraa <pabasara.was@gmail.com>
2022-11-02 17:07:02 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+//import css file from style sheets directory
+import styles from "./style_sheets/App.module.css";
+
+//import components from the components folder
+import Add from "./components/AddRoutes";
+import All from "./components/AllRoutes";
+import Edit from "./components/EditRoutes";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <div>
+          <Switch>
+
+            <Route
+              path="/add"
+              component={Add} 
+            />
+
+            <Route
+              path="/all"
+              component={All} 
+            />
+
+            <Route
+              path="/edit/:id"
+              component={Edit} 
+            />
+
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
